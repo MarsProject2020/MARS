e25443b746460db421a56d2fd51f85f14236ac1b
RaneeTurki <ranee.turki@gmail.com>
2021-01-12 21:00:47 +0100

    First commit

---
+++ lib/screens/widgets/onboarding/pages/education/education_dark_card_content.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:mars/constants.dart';
+
+//import 'package:mars/screens/widgets/onboarding/icon_container.dart';
+class EducationDarkCardContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      Icons.laptop_mac,
+      color: kWhite,
+      size: 96.0,
+    );
+  }
+}
---
+++ lib/screens/widgets/onboarding/pages/education/education_light_card_content.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:mars/constants.dart';
+import 'package:mars/screens/widgets/onboarding/icon_container.dart';
+
+class EducationLightCardContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        IconContainer(
+          icon: Icons.brush,
+          padding: kPaddingS,
+        ),
+        IconContainer(
+          icon: Icons.camera_alt,
+          padding: kPaddingM,
+        ),
+        IconContainer(
+          icon: Icons.straighten,
+          padding: kPaddingS,
+        ),
+      ],
+    );
+  }
+}
